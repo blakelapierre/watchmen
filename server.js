f07453f395d831e573cac37904b5e3079aa6bf0e
Blake La Pierre <blakelapierre@gmail.com>
2014-03-18 19:44:14 -0400

    tests ok so far

--- server.js
+++ server.js
@@ -1,14 +1,43 @@
 var githubhook = require('githubhook'),
-    _ = require('underscore');
-    
+    _ = require('underscore'),
+    sys = require('sys'),
+    childProcess = require('child_process');
+  
+var watching = {
+	"facerace": {
+		location: '/home/facerace/facerace',
+		commands: [
+			'grunt localDeploy'
+		]
+	},
+	"watchmen_test": {
+		location: '/home/watchmen/watchmen_test',
+		commands: [
+			'echo "hello world"'
+		]
+	}
+};
+
+function puts(error, stdout, stderr) { sys.puts(stdout); };
+
+function exec(location, command) { 
+    console.log('executing ' + command + ' at: ' + location);
+    childProcess.exec('cd ' + location + ' && ' + command, puts);
+};
 
 exports.startServer = function (config, callback) {
     var github = githubhook(config.server);
 
     github.listen();
     
-    github.on('event', function(repo, ref, data) {
-        
+    github.on('push', function(repo, ref, data) {
+    	var project = watching[repo];
+
+	    if (project) {
+    		_.each(project.commands, function(command) {
+                exec(project.location, command);
+		    });
+	    }
     });
 
     callback(github);
