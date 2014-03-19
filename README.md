191cc6c727b33ca7bab640e35a6c2c4ae9e94a4f
Blake La Pierre <blakelapierre@gmail.com>
2014-03-19 19:08:58 -0400

    basic poc

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ WATCHMEN
 ========
 
 insta-deploy-on-commit for your node projects
+
+this only really likes deploying once right now. i'll get it fixed up soon
--- server.js
+++ server.js
@@ -2,36 +2,40 @@ var githubhook = require('githubhook'),
     _ = require('underscore'),
     sys = require('sys'),
     fs = require('fs'),
+    path = require('path'),
     childProcess = require('child_process');
 
 var watching = {};
 
 function puts(error, stdout, stderr) { sys.puts(stdout); };
 
-function exec(location, command) { 
-    console.log('executing ' + command + ' at: ' + location);
-    return childProcess.exec('cd ' + location + ' && ' + command, puts);
+function exec(command) { 
+    console.log('executing command: ' + command);
+    return childProcess.exec(command, puts);
 };
 
 exports.startServer = function (config, callback) {
     var github = githubhook(config.server);
 
-    var retrieve = function(repo) {
+    var retrieve = function(repo, data) {
         var project = watching[repo] || {
             name: repo,
             location: '/apps/' + repo,
-            initialized: false
+            remoteLocation: data.repository.url,
+            initialized: false,
+            nextDeploymentID: 0
         };
 
         watching[repo] = project;
 
         if (!project.initialized) initialize(project);
+        
+        return project;
     };
 
     var initialize = function(project) {
-        if (!fs.existsSync(project.location)) {
-            fs.mkdirSync(project.location, 0775);
-        }
+        var location = path.join(project.location, project.nextDeploymentID.toString());
+        project.deploymentCommand = 'mkdir -p ' + location + ' && cd ' + location + ' && git init && git remote add origin ' + project.remoteLocation + ' && git pull origin master && npm install && bower install && grunt localDeploy';
     };
     
     var deploy = function(project) {
@@ -40,18 +44,20 @@ exports.startServer = function (config, callback) {
             project.deployment.kill();
         }
         else {
+            var deploymentID = project.nextDeploymentID++;
+
             project.redeploy = false;
-            project.deploy = exec(project.location, 'git pull origin');
-            project.deploy.on('exit', function() {
-                delete project.deploy;
+            project.deployment = exec(project.deploymentCommand);
+            project.deployment.on('exit', function() {
+                delete project.deployment;
                 if (project.redeploy) deploy(project);
             });
         }
     };
 
     github.on('push', function(repo, ref, data) {
-    	var project = retrieve(repo);
-
+console.dir(data);
+        var project = retrieve(repo, data);
         deploy(project);
     });
 
