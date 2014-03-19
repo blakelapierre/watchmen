210cc4c80a8fa909712059222626e8421406494c
Blake La Pierre <blakelapierre@gmail.com>
2014-03-19 15:21:42 -0700

    pushing this and moving over to deploy server for more dev work. yes it's all sync, not going to try to figure out how to promisize/generatorize/betterize right now.

--- server.js
+++ server.js
@@ -1,22 +1,10 @@
 var githubhook = require('githubhook'),
     _ = require('underscore'),
     sys = require('sys'),
+    fs = require('fs'),
     childProcess = require('child_process');
-  
-var watching = {
-	"facerace": {
-		location: '/home/facerace/facerace',
-		commands: [
-			'grunt localDeploy'
-		]
-	},
-	"watchmen_test": {
-		location: '/home/watchmen/watchmen_test',
-		commands: [
-			'echo "hello world"'
-		]
-	}
-};
+
+var watching = {};
 
 function puts(error, stdout, stderr) { sys.puts(stdout); };
 
@@ -25,35 +13,49 @@ function exec(location, command) {
     return childProcess.exec('cd ' + location + ' && ' + command, puts);
 };
 
-
-var deploy = function(project) {
-    project.redeploy = false;
-    project.deploy = exec(project.location, 'git pull origin && npm update && bower update && grunt localDeploy');
-    project.deploy.on('exit', function() {
-        delete project.deploy;
-        if (project.redeploy) deploy(project);
-    });
-};
-
 exports.startServer = function (config, callback) {
     var github = githubhook(config.server);
 
-    github.listen();
-    
-    github.on('push', function(repo, ref, data) {
-    	var project = watching[repo];
+    var retrieve = function(repo) {
+        var project = watching[repo] || {
+            name: repo,
+            location: '/apps/' + repo,
+            initialized: false
+        };
 
+        watching[repo] = project;
 
-        if (project) {
-            if (project.deploy) {
-                project.redeploy = true;
-                project.deploy.kill();
-            }
-            else {
-                deploy(project);
-            }
+        if (!project.initialized) initialize(project);
+    };
+
+    var initialize = function(project) {
+        if (!fs.existsSync(project.location)) {
+            fs.mkdirSync(project.location, 0775);
         }
+    };
+    
+    var deploy = function(project) {
+        if (project.deployment) {
+            project.redeploy = true;
+            project.deployment.kill();
+        }
+        else {
+            project.redeploy = false;
+            project.deploy = exec(project.location, 'git pull origin');
+            project.deploy.on('exit', function() {
+                delete project.deploy;
+                if (project.redeploy) deploy(project);
+            });
+        }
+    };
+
+    github.on('push', function(repo, ref, data) {
+    	var project = retrieve(repo);
+
+        deploy(project);
     });
+
+    github.listen();
 
     callback(github);
 };
